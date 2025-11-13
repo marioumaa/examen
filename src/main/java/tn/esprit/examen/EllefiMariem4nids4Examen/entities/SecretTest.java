3f1fb129c04be8e7d9267ab4374577362b071393
mariem ellefi <mariem.ellefi@esprit.tn>
2025-11-13 09:45:17 +0100

    Ajout de la nouvelle fonctionnalité X

--- src/main/java/tn/esprit/examen/EllefiMariem4nids4Examen/entities/SecretTest.java
+++ src/main/java/tn/esprit/examen/EllefiMariem4nids4Examen/entities/SecretTest.java
@@ -3,5 +3,5 @@ package tn.esprit.examen.EllefiMariem4nids4Examen.entities;
 public class SecretTest {
     private String apiKey = "AIzaSyA-EXAMPLE-KEY-1234567890";
     private String password = "P@ssw0rd123!";
-    private String name;
+
 }
